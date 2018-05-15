ca71ea6645210c0299b5c7ed9126b0fb0b173eda
Vargas Gonzalez <Y4072@csx.com>
2018-05-15 16:39:00 -0400

    post request using http and firebase

---
+++ src/app/server.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core'
+import { Http } from '@angular/http'
+
+@Injectable()
+export class ServerService {
+    constructor(private http: Http) {}
+
+    storeServers(servers: any[]) {
+        //This will only create an observable, but is not sending it yet
+        return this.http.post('https://http-tamo.firebaseio.com/data.json', servers);
+    }
+
+}
